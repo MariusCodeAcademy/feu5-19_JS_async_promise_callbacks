f1faca581daca80a2bbbb7cff11690b6a705b8ce
Marius CA <marius@example.com>
2023-03-03 10:39:58 +0200

    cb posts start

--- js/callbacks.js
+++ js/callbacks.js
@@ -1,14 +1,47 @@
 'use strict';
 console.log('callbacks.js file was loaded');
 
-function getPost1() {
+function getPost1(callback) {
   setTimeout(() => {
-    console.log('Post 1');
+    console.log('Header content');
+    // iskviesti main kontent
+    callback();
   }, 3000);
 }
 
 // sukurti getPost2() kuri atspausdina po 2 sekundziu
+function getPost2() {
+  setTimeout(() => {
+    console.log('Main content');
+  }, 2000);
+}
 // sukurti getPost3() kuri atspausdina po 1.5 sekundziu
+function getPost3() {
+  setTimeout(() => {
+    console.log('Footer');
+  }, 1500);
+}
 
+getPost1(getPost2);
+// getPost2();
+// getPost3();
 // iskviesti visas funkcijas is eiles
 // paziureti kokia eiles tvarka atsispausdina logai
+
+// callback pavayzdys
+function makeNum(callback) {
+  const rez = callback(5);
+  return rez;
+}
+
+function double(num) {
+  return num * 2;
+}
+function tripple(num) {
+  return num * 3;
+}
+
+const n1 = makeNum(tripple);
+console.log('n1 ===', n1);
+
+console.log(double(10));
---
+++ js/posts.js
@@ -0,0 +1,7 @@
+'use strict';
+console.log('posts.js file was loaded');
+
+const posts = [
+  { title: 'Post One', body: 'This is post One body' },
+  { title: 'Post Two', body: 'This is post Two body' },
+];
