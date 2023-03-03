cf34b92e93e2c4138c1b2806fd8587edeb38a9c3
Marius CA <marius@example.com>
2023-03-03 10:52:59 +0200

    post problem

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import tick from './modules/clock.js';
 
 console.log('main.js file was loaded');
-
+tick();
 setInterval(tick, 1000);
--- js/posts.js
+++ js/posts.js
@@ -1,7 +1,34 @@
 'use strict';
 console.log('posts.js file was loaded');
 
+// taikomes
+const appEl = document.getElementById('app');
+
 const posts = [
   { title: 'Post One', body: 'This is post One body' },
   { title: 'Post Two', body: 'This is post Two body' },
 ];
+
+// gauti postus imituojam kad truka 1.5sek juos gauti
+function getPosts() {
+  setTimeout(() => {
+    appEl.innerHTML = '';
+    posts.forEach(({ title, body }) => {
+      const pEl = document.createElement('p');
+      pEl.textContent = `Title: ${title}. ${body}`;
+      appEl.append(pEl);
+    });
+  }, 1500);
+}
+
+// create post funkcija kuti ideda nauja posta i pos masyva
+function createPost(newPostObj) {
+  setTimeout(() => {
+    posts.push(newPostObj);
+    console.log('created Post');
+  }, 2500);
+}
+
+createPost({ title: 'Post Three', body: 'This is post Three body' });
+
+getPosts();
