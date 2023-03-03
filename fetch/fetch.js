063db5f7b291a4071c2661a520d7c9076f93478b
Marius CA <marius@example.com>
2023-03-03 15:26:39 +0200

    makeOnePost

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,6 +1,9 @@
 'use strict';
 console.log('fetch.js file was loaded');
 
+// taikomes
+const appEl = document.getElementById('app');
+
 // fetch(url) - grazina promise, parsiuncia get requesta
 
 const urlPosts = 'https://jsonplaceholder.typicode.com/posts';
@@ -30,5 +33,28 @@ getPosts().then((posts) => renderPosts(posts));
 function renderPosts(arr) {
   console.log('i magically draw some html with these posts', arr);
   // sukti ciklas ir generuoti korteles htmle
+  arr.forEach((pObj) => {
+    const oneEl = makeOnePost(pObj);
+    appEl.append(oneEl);
+  });
   // 3 korteles per ploti
+}
+
+/* 
+<li data-id="id" class="postItem">
+  <h3>title</h3>
+  <p><em>user id: 45</em></p>
+  <p class="text">body</p>
+</li>
+*/
+function makeOnePost(obj) {
+  const liEl = document.createElement('li');
+  liEl.className = 'postItem';
+  liEl.dataset.id = obj.id;
+  liEl.innerHTML = `
+  <h3>${obj.title}</h3>
+  <p><em>user id: ${obj.userId}</em></p>
+  <p class="text">${obj.body}</p>
+  `;
+  return liEl;
 }
